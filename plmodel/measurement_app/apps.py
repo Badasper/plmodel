f75363315d5909bc4c4b00a21e8b0e2c328fcdf8
Alexander Yakovlev <job.yakovlev.alexander@gmail.com>
2019-09-16 21:23:14 +0700

    add urls to measurementapp

--- plmodel/measurement_app/apps.py
+++ plmodel/measurement_app/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class MeasurementConfig(AppConfig):
+class MeasurementAppConfig(AppConfig):
     name = 'measurement'
--- plmodel/plmodel/urls.py
+++ plmodel/plmodel/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('measurement/', include('measurement.urls')),
+    path('measurement/', include('measurement_app.urls')),
     path('admin/', admin.site.urls),
 ]
